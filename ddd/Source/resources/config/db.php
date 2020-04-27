93cd559f1a6a5dd7fa828a8efc4cefc04971f0df
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2020-04-27 17:56:16 +0100

    4:55pm

--- ddd/Source/resources/config/db.php
+++ ddd/Source/resources/config/db.php
@@ -30,16 +30,3 @@
 }
 
 
-/*i wanna write a clean code with class and db for all my calls 
-i also want to remove any unnecessary files i don't need and folders that are no longer in 
-use to free the system, i also a read me file to document each progress as i go forward
-this job might be shity and my employer might be a turd but i am not. i God a good explanation
-of my talents and gifts. millions will be better for it, lots of people will have a diff
-ecommernce experience cuz of me, i owe God that */
-
-/**
- * im quickly gonna write a neat auth with forget password and login with google button that actually works
- * and i would move on from there, with well define comments so in future i can improve upon it
- * i think the best way for me to do that is by looking at a real auth written by a senior dev
- * and model mine after but keeping it simple and the lines very short .....!
- */
